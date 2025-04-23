b390a9b0619056ba8d9a7a62b5959bea5d229e81
tgen0123 <nguyenan411409@gmail.com>
2025-04-24 02:30:30 +0700

    Update product.route.ts

--- backend/src/routes/v1/product.route.ts
+++ backend/src/routes/v1/product.route.ts
@@ -4,7 +4,7 @@ import { ProductControllers } from "../../modules/product/product.controller";
 const router = express.Router();
 
 router.post("/", ProductControllers.createProduct);
-router.get("/", ProductControllers.getAllProduct);
+router.get("/", ProductControllers.getAllProducts);
 router.get("/:productId", ProductControllers.getSingleProduct);
 router.put("/:productId", ProductControllers.updateSingleProduct);
 router.delete("/:productId", ProductControllers.deleteOneProduct);

4b22585b8ed8ccbc3d6df7ecfc9bf72dcb81413c
tgen0123 <nguyenan411409@gmail.com>
2025-04-24 22:58:20 +0700

    Update page.tsx

--- frontend/src/app/merchant/dashboard/product/page.tsx
+++ frontend/src/app/merchant/dashboard/product/page.tsx
@@ -2,43 +2,27 @@ import React, { useMemo } from 'react'
 import NextLink from 'next/link'
 import useSWR from 'swr'
 import { AddOutlined} from '@mui/icons-material'
-import { Box, Button, CardMedia, Chip, Grid, Link } from '@mui/material'
+import { Button, CardMedia, Chip, Grid, Link } from '@mui/material'
 import { DataGrid, GridColDef } from '@mui/x-data-grid'
+import { Container, Typography, Box } from '@mui/material';
 import Layout from '../layout'
 import { FullScreenLoading } from '../../../../components/ui/fullScreenLoading'
 import { IProduct } from '../../../../../../backend/src/modules/product/product.interface'
 
 const columns: GridColDef[] = [
-    {
-        field: 'photoUrls', 
-        headerName: 'Photo',
-        renderCell: ({ row }: { row: { slug: string; name: string; photoUrls: string } }) => {
-            return (
-                <a href={`/products/${ row.slug }`} target='_blank' rel='noreferrer'>
-                    <CardMedia 
-                        component='img'
-                        className='fadeIn'
-                        alt={ row.name }
-                        image={`${row.photoUrls}`}
-                    />
-                </a>
-            )
-        }
-    },
-    {
-        field: 'name', 
-        headerName: 'Name', 
-        width: 250, 
-        renderCell: ({ row }: { row: { slug: string; name: string } }) => {
-            return (
-                <NextLink href={`/admin/products/${ row.slug }`} passHref>
-                    <Link underline='always'>
-                        {row.name}
-                    </Link>
-                </NextLink>
-            )
-        }
-    },
+    {field: 'photoUrls', headerName: 'Image', width: 100, renderCell: (params) => (
+        <CardMedia
+            component='img'
+            alt={params.row.name}
+            image={params.row.photoUrls[0]}
+            sx={{ borderRadius: 1, height: 50, width: 50 }}
+        />
+    )},
+    {field: 'name', headerName: 'Name', width: 250, renderCell: (params) => (
+        <Link component={NextLink} href={`/merchant/products/${params.row.merchant}`} underline='always'>
+            {params.row.name}
+        </Link>
+    )},
     {field: 'description', headerName: 'Description', width: 250},
     {field: 'merchant', headerName: 'Merchant', width: 250},
     {field: 'status', headerName: 'Status'},
@@ -49,15 +33,14 @@ const columns: GridColDef[] = [
 ]
 
 
-const ProductPage = () => {
+export default function ProductPage(): React.JSX.Element {
 
-    const {data, error} = useSWR<IProduct[]>('/api/admin/products')
+    const {data, error} = useSWR<IProduct[]>('/merchant/dashboard/products')
 
     const rows = useMemo(()=> { 
         if ( data ) {
             return (
                 data.map((product: IProduct): IProduct => ({
-                    slug: product.slug,
                     photoUrls: [product.photoUrls[0]],
                     name: product.name,
                     description: product.description,
@@ -74,33 +57,37 @@ const ProductPage = () => {
         }
     }, [data])
   return ( 
-    <Layout>
-        <Box display='flex' justifyContent='end' sx={{ mb: 2 }}>
-            <Button
-                startIcon={<AddOutlined />}
-                color='secondary'
-                href='/admin/products/create'
-            >
-                Create Product
-            </Button>
-        </Box>
-        {!data && !error 
-            ? (<FullScreenLoading />) 
-            : ( <Grid container className='fadeIn' item xs={12} sx={{ height:650, width: '100%' }}>
-                    <DataGrid
-                        rows={ rows }
-                        columns={ columns }
-                        initialState={{
-                            pagination: {
-                                paginationModel: { pageSize: 10 },
-                            },
-                        }}
-                        pageSizeOptions={[10]}
-                    />
-                </Grid>)}
-        
-    </Layout>
+    <Container maxWidth={false}>
+      <Box sx={{ mt: 3 }}>
+        <Typography variant="h4" gutterBottom>
+            Products
+            <Chip label={data?.length} color='secondary' variant='outlined' sx={{ ml: 2 }} />
+            <Box display='flex' justifyContent='end' sx={{ mb: 2 }}>
+                <Button
+                    startIcon={<AddOutlined />}
+                    color='secondary'
+                    href='/merchant/products/create'
+                >
+                    Create Product
+                </Button>
+            </Box>
+            {!data && !error 
+                ? (<FullScreenLoading />) 
+                : ( <Grid container className='fadeIn' item xs={12} sx={{ height:650, width: '100%' }}>
+                        <DataGrid
+                            rows={ rows }
+                            columns={ columns }
+                            initialState={{
+                                pagination: {
+                                    paginationModel: { pageSize: 10 },
+                                },
+                            }}
+                            pageSizeOptions={[10]}
+                        />
+                    </Grid>)}    
+        </Typography>
+      </Box>
+    </Container>
   )
 }
 
-export default ProductPage
